55fc437fbf596cee187a75c04f76d34bc293b36d
zsiddiqui2 <zasdqui@gmail.com>
2014-06-09 20:25:49 -0500

    First Commit

---
+++ Init.java
@@ -0,0 +1,14 @@
+
+/**
+ * First assignment for COMP 312.
+ * 
+ * @zsiddiqui2 (Zafar A. Siddiui) 
+ */
+public class Init
+{
+    public static void main(String[] args)
+    {
+        System.out.println("This is my first assignment for 312!");
+    }
+
+}
